fa5790b68d3e0a9a98cd24916d9b962b1ce04f66
DavisVaughan <davis@rstudio.com>
2019-08-27 18:32:35 -0400

    Bring tests up to date

--- tests/testthat/test-calendar.R
+++ tests/testthat/test-calendar.R
@@ -2,7 +2,8 @@ test_that("can create a default calendar", {
   cal <- calendar()
   expect_is(cal, "calendar")
   expect_equal(cal$name, "united_states")
-  expect_equal(cal$holidays, new_date())
+  expect_equal(cal$added_holidays, new_date())
+  expect_equal(cal$removed_holidays, new_date())
 })
 
 test_that("can create a calendar with a different name", {
@@ -38,20 +39,65 @@ test_that("calendar printing", {
   expect_known_output(print(cal), file = test_path("output/print-calendar-argentina.txt"))
 
   cal <- calendar()
-  holiday <- as.Date("2019-01-01")
+  holiday <- as.Date("2019-01-02")
   cal <- holidays_add(cal, holiday)
   expect_known_output(print(cal), file = test_path("output/print-calendar-holiday.txt"))
 
   cal <- calendar(calendars$argentina)
-  holidays <- as.Date(c("2019-01-01", "2019-01-03", "2019-01-02"))
+  holidays <- as.Date(c("2019-01-02", "2019-01-04", "2019-01-03"))
   cal <- holidays_add(cal, holidays)
   expect_known_output(print(cal), file = test_path("output/print-calendar-holidays.txt"))
 
   cal <- calendar(calendars$argentina)
-  many_holidays <- as.Date("2019-01-01") + 0:10
+  many_holidays <- as.Date("2019-01-02") + 0:10
   cal <- holidays_add(cal, many_holidays)
   expect_known_output(print(cal), file = test_path("output/print-calendar-many-holidays.txt"))
+
+  cal <- calendar()
+  holiday <- as.Date("2019-01-01")
+  cal <- holidays_remove(cal, holiday)
+  expect_known_output(print(cal), file = test_path("output/print-calendar-removed-holiday.txt"))
+
+  cal <- calendar()
+  holiday <- as.Date(c("2019-01-01", "2018-01-01", "2017-01-01"))
+  cal <- holidays_remove(cal, holiday)
+  expect_known_output(print(cal), file = test_path("output/print-calendar-removed-holidays.txt"))
+
+  cal <- calendar()
+  holiday <- as.Date("2019-01-01") - lubridate::years(0:10)
+  cal <- holidays_remove(cal, holiday)
+  expect_known_output(print(cal), file = test_path("output/print-calendar-many-removed-holidays.txt"))
+
+  cal <- calendar()
+  holiday <- as.Date("2019-01-01") - lubridate::years(0:10)
+  cal <- holidays_remove(cal, holiday)
+  holiday <- as.Date("2019-01-02") - lubridate::years(0:10)
+  cal <- holidays_add(cal, holiday)
+  expect_known_output(print(cal), file = test_path("output/print-calendar-mixed-holidays.txt"))
 })
+
+# ------------------------------------------------------------------------------
+
+test_that("custom calendar printing", {
+  cal <- custom_calendar()
+  expect_known_output(print(cal), file = test_path("output/print-custom-calendar.txt"))
+
+  cal <- custom_calendar(weekends = weekday)
+  expect_known_output(print(cal), file = test_path("output/print-custom-calendar-weekends.txt"))
+
+  cal <- custom_calendar()
+  holiday <- as.Date("2019-01-02")
+  cal <- holidays_add(cal, holiday)
+  expect_known_output(print(cal), file = test_path("output/print-custom-calendar-holiday.txt"))
+
+  cal <- custom_calendar()
+  many_holidays <- as.Date("2019-01-02") + 0:10
+  cal <- holidays_add(cal, many_holidays)
+  expect_known_output(print(cal), file = test_path("output/print-custom-calendar-many-holidays.txt"))
+})
+
+
+# ------------------------------------------------------------------------------
 
 test_that("calendars object", {
   expect_is(calendars, "list")
--- tests/testthat/test-holidays.R
+++ tests/testthat/test-holidays.R
@@ -1,59 +1,121 @@
+# ------------------------------------------------------------------------------
+# Adding holidays
+
 test_that("can add a holiday", {
   cal <- calendar()
-  cal <- holidays_add(cal, "2019-01-01")
-
-  expect_equal(cal$holidays, as.Date("2019-01-01"))
-
   cal <- holidays_add(cal, "2019-01-02")
-  expect_equal(cal$holidays, as.Date(c("2019-01-01", "2019-01-02")))
+
+  expect_equal(holidays_added(cal), as.Date("2019-01-02"))
+
+  cal <- holidays_add(cal, "2019-01-03")
+  expect_equal(holidays_added(cal), as.Date(c("2019-01-02", "2019-01-03")))
 })
 
 test_that("can add multiple holidays at once", {
   cal <- calendar()
-  cal <- holidays_add(cal, c("2019-01-01", "2019-01-02"))
+  cal <- holidays_add(cal, c("2019-01-02", "2019-01-03"))
 
-  expect_equal(cal$holidays, as.Date(c("2019-01-01", "2019-01-02")))
+  expect_equal(holidays_added(cal), as.Date(c("2019-01-02", "2019-01-03")))
 })
 
 test_that("holidays are sorted", {
   cal <- calendar()
-  cal <- holidays_add(cal, c("2019-01-02", "2019-01-01"))
+  cal <- holidays_add(cal, c("2019-01-03", "2019-01-02"))
 
-  expect_equal(cal$holidays, as.Date(c("2019-01-01", "2019-01-02")))
+  expect_equal(holidays_added(cal), as.Date(c("2019-01-02", "2019-01-03")))
 })
 
 test_that("holidays are unique", {
   cal <- calendar()
-  cal <- holidays_add(cal, c("2019-01-01", "2019-01-01"))
+  cal <- holidays_add(cal, c("2019-01-02", "2019-01-02"))
 
-  expect_equal(cal$holidays, as.Date("2019-01-01"))
+  expect_equal(holidays_added(cal), as.Date("2019-01-02"))
 
+  cal <- holidays_add(cal, "2019-01-02")
+
+  expect_equal(holidays_added(cal), as.Date("2019-01-02"))
+})
+
+test_that("existing holidays are not added", {
+  cal <- calendar()
   cal <- holidays_add(cal, "2019-01-01")
 
-  expect_equal(cal$holidays, as.Date("2019-01-01"))
+  expect_equal(holidays_added(cal), new_date())
+
+  cal <- holidays_add(cal, "2019-01-02")
+  cal <- holidays_add(cal, "2019-01-02")
+
+  expect_equal(holidays_added(cal), as.Date("2019-01-02"))
 })
+
+test_that("can both add to added and remove from removed in one step", {
+  cal <- calendar()
+  cal <- holidays_remove(cal, "2019-01-01")
+  cal <- holidays_add(cal, c("2019-01-01", "2019-01-02"))
+
+  expect_equal(holidays_added(cal), as.Date("2019-01-02"))
+})
+
+test_that("can look at added holidays", {
+  cal <- calendar()
+  cal <- holidays_add(cal, "2019-01-02")
+
+  expect_equal(holidays_added(cal), cal$added_holidays)
+})
+
+test_that("weekends are counted as holidays", {
+  cal <- calendar()
+  cal <- holidays_add(cal, "2019-01-05")
+  expect_equal(holidays_added(cal), new_date())
+})
+
+# ------------------------------------------------------------------------------
+# Removing holidays
 
 test_that("holidays can be removed", {
   cal <- calendar()
-  cal <- holidays_add(cal, "2019-01-01")
-  cal <- holidays_remove(cal, "2019-01-01")
+  cal <- holidays_add(cal, "2019-01-02")
+  cal <- holidays_remove(cal, "2019-01-02")
 
-  expect_equal(holidays_custom(cal), new_date())
+  expect_equal(holidays_added(cal), new_date())
 })
 
-test_that("removing a non-existant holiday is a no-op", {
+test_that("non-existant holidays are not removed", {
+  cal <- calendar()
+  cal <- holidays_remove(cal, "2019-01-02")
+
+  expect_equal(holidays_removed(cal), new_date())
+})
+
+test_that("can remove pre-existing holidays", {
   cal <- calendar()
   cal <- holidays_remove(cal, "2019-01-01")
 
-  expect_equal(holidays_custom(cal), new_date())
+  expect_equal(holidays_removed(cal), as.Date("2019-01-01"))
 })
 
-test_that("can look at custom holidays", {
+test_that("can both remove from added and add to removed in one step", {
   cal <- calendar()
-  cal <- holidays_add(cal, "2019-01-01")
+  cal <- holidays_add(cal, "2019-01-02")
+  cal <- holidays_remove(cal, c("2019-01-01", "2019-01-02"))
 
-  expect_equal(holidays_custom(cal), cal$holidays)
+  expect_equal(holidays_removed(cal), as.Date("2019-01-01"))
 })
+
+test_that("can look at removed holidays", {
+  cal <- calendar()
+  cal <- holidays_remove(cal, "2019-01-01")
+
+  expect_equal(holidays_removed(cal), cal$removed_holidays)
+})
+
+test_that("weekends can be removed, since they are holidays", {
+  cal <- calendar()
+  cal <- holidays_remove(cal, "2019-01-05")
+  expect_equal(holidays_removed(cal), as.Date("2019-01-05"))
+})
+
+# ------------------------------------------------------------------------------
 
 test_that("can look at all holidays in a calendar", {
   cal <- calendar()
@@ -73,17 +135,6 @@ test_that("custom holidays are included in all holiday query", {
   holidays <- holidays_all(cal)
 
   expect_true(vec_in(as.Date("2019-01-07"), holidays))
-})
-
-test_that("custom holidays are not included if they are weekends and `weekends = FALSE`", {
-  cal <- calendar()
-  cal <- holidays_add(cal, "2019-01-05")
-
-  holidays <- holidays_all(cal, weekends = FALSE)
-  expect_false(vec_in(as.Date("2019-01-05"), holidays))
-
-  holidays <- holidays_all(cal, weekends = TRUE)
-  expect_true(vec_in(as.Date("2019-01-05"), holidays))
 })
 
 test_that("can get all holidays for all calendars", {
@@ -116,7 +167,7 @@ test_that("can get holidays between dates", {
   )
 })
 
-test_that("custom holidays are included", {
+test_that("custom holidays are included when searching for holidays", {
   cal <- calendar()
   cal <- holidays_add(cal, "2019-01-02")
 
